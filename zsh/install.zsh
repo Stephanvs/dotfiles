b6a8d1c0454f1849180ef58b7b2479d94d7fc7b2
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-05-12 21:15:44 +0200

    zsh: fix location for zsh theme

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,2 +1,2 @@
 ln --symbolic $DOTFILES/zsh/zshrc $HOME/.zshrc
-ln --symbolic $DOTFILES/zsh/honukai.zsh-theme $HOME/.oh-my-zsh/honukai.zsh-theme
+ln --symbolic $DOTFILES/zsh/honukai.zsh-theme $HOME/.oh-my-zsh/themes/honukai.zsh-theme
