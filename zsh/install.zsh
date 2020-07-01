f2910582789bd8ccffb6d27c0db4e1ee2cfbbe1a
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-07-01 17:04:32 +0200

    updated skhd and yabai rc files

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,2 +1,2 @@
-ln --symbolic $DOTFILES/zsh/zshrc $HOME/.zshrc
-ln --symbolic $DOTFILES/zsh/honukai.zsh-theme $HOME/.oh-my-zsh/themes/honukai.zsh-theme
+ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
+ln -s $DOTFILES/zsh/honukai.zsh-theme $HOME/.oh-my-zsh/themes/honukai.zsh-theme
