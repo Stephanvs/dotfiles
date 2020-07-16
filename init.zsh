eec69e1cfe2f913b2472dbff0319dff920837a65
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-07-16 13:24:40 +0200

    fix indentation

--- init.zsh
+++ init.zsh
@@ -9,32 +9,32 @@ DEBUG_COLOR="\e[38m"
 RESET_COLOR="\e[m"
 
 _message() {
-    msg=$1
-    color=$2
-    printf '%b%b%b\n' "${color}" "${msg}" "${RESET_COLOR}"
+  msg=$1
+  color=$2
+  printf '%b%b%b\n' "${color}" "${msg}" "${RESET_COLOR}"
 }
 
 success() {
-    _message "${1}" "$SUCCESS_COLOR"
+  _message "${1}" "$SUCCESS_COLOR"
 }
 
 info() {
-    _message "${1}" "$INFO_COLOR"
+  _message "${1}" "$INFO_COLOR"
 }
 
 debug() {
-    _message "${1}" "$DEBUG_COLOR"
+  _message "${1}" "$DEBUG_COLOR"
 }
 
 warn() {
-    _message "${1}" "$WARN_COLOR"
+  _message "${1}" "$WARN_COLOR"
 }
 
 error() {
-    _message "error: ${1}" "$ERROR_COLOR"
+  _message "error: ${1}" "$ERROR_COLOR"
 }
 
 fail() {
-    _message "failed: ${1}" "$ERROR_COLOR"
-    exit 1
+  _message "failed: ${1}" "$ERROR_COLOR"
+  exit 1
 }
