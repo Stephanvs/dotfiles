0a072d8a557406b1593c11a3a199ce379356ffa7
Stephan van Stekelenburg <stephan@hayman.io>
2020-10-20 21:08:42 +0200

    compton is now known as picom

--- compton/install.zsh
+++
@@ -1,2 +0,0 @@
-mkdir -p $HOME/.config/compton
-ln -f --symbolic $DOTFILES/compton/compton.conf $HOME/.config/compton.conf
---
+++ picom/install.zsh
@@ -0,0 +1,2 @@
+mkdir -p $HOME/.config/picom
+ln -f --symbolic $DOTFILES/picom/picom.conf $HOME/.config/picom.conf
