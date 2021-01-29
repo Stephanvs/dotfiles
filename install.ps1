aa390bbd150795aa640a9ad85fa27a6ac7f7f0c6
Stephan van Stekelenburg <stephan@hayman.io>
2021-01-29 17:07:27 +0100

    chore: find all install files recursively

---
+++ install.ps1
@@ -0,0 +1,8 @@
+# Find all `install.ps1` files recursively
+$files = Get-ChildItem -Recurse -Filter install.ps1
+foreach ($f in $files) {
+  Write-Output "executing: $f"
+
+  # Invoke the install script
+  & $f
+}
