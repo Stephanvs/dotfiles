0cffe18b5dd0b9b47e7da050a514765b64ab3ead
Stephan van Stekelenburg <stephan@hayman.io>
2021-11-09 16:49:19 +0100

    install oh-my-posh

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -1,3 +1,8 @@
+# Install Terminal Icons
 Install-Module -Name Terminal-Icons -Repository PSGallery
 
+# Install OhMyPosh
+winget install JanDeDobbeleer.OhMyPosh
+
+# Create Symbolic link for powershell profile
 New-Item -ItemType SymbolicLink -Path ./profile.ps1 -Target $PROFILE
