65f122acc9e1a866ab0542c1d95c9bf5b7f265c9
Stephan van Stekelenburg <Stephanvs@users.noreply.github.com>
2021-11-30 22:53:11 +0100

    feat(win): install software script via winget

---
+++ windows/pave-new-machine.ps1
@@ -0,0 +1,8 @@
+winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
+winget install Microsoft.VisualStudio.2022.Professional --silent --accept-package-agreements --accept-source-agreements
+winget install AgileBits.1Password --silent
+winget install GitHub.GitHubDesktop --silent
+winget install Git.Git --silent
+
+winget install WhatsApp.WhatsApp --silent
+winget install SlackTechnologies.Slack --silent
