c95a20fec200233b7ef4bf2521c4192a0ab14e7c
Stephan van Stekelenburg <Stephanvs@users.noreply.github.com>
2021-11-30 23:10:23 +0100

    feat(win): install rider as editor

--- windows/pave-new-machine.ps1
+++ windows/pave-new-machine.ps1
@@ -1,9 +1,14 @@
+# Dev editors
 winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
 winget install Microsoft.VisualStudio.2022.Professional --silent --accept-package-agreements --accept-source-agreements
+winget install JetBrains.Rider --silent
+
+# Generic tools
 winget install AgileBits.1Password --silent
 winget install GitHub.GitHubDesktop --silent
 winget install Git.Git --silent
 
+# Communication tools
 winget install WhatsApp.WhatsApp --silent
 winget install SlackTechnologies.Slack --silent
 winget install Microsoft.Teams --silent
