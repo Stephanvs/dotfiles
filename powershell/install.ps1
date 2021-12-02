d51b9820247740ae3293a182c403e855b1cadfe5
Stephan van Stekelenburg <Stephanvs@users.noreply.github.com>
2021-12-02 14:35:29 +0100

    feat(pwsh): install posh silently

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -2,7 +2,7 @@
 Install-Module -Name Terminal-Icons -Repository PSGallery
 
 # Install OhMyPosh
-winget install JanDeDobbeleer.OhMyPosh
+winget install JanDeDobbeleer.OhMyPosh --silent
 
 # Create Symbolic link for powershell profile
 New-Item -ItemType SymbolicLink -Path $PROFILE -Target $PSScriptRoot/profile.ps1 -Force
