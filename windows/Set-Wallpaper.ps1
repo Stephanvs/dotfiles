c636002f53fa200e2da4984b2e467fae8d341214
Stephan van Stekelenburg <Stephanvs@users.noreply.github.com>
2021-12-08 10:05:46 +0100

    feat(win): change wallpaper

--- windows/Set-Wallpaper.ps1
+++ windows/Set-Wallpaper.ps1
@@ -1,12 +1,12 @@
 Set-ItemProperty `
   -Path "HKCU:Control Panel\Desktop" `
   -Name WallPaper `
-  -Value (Get-Item ..\wallpapers\mars.jpg | Resolve-Path).ProviderPath
+  -Value (Get-Item ..\wallpapers\nasa.jpg | Resolve-Path).ProviderPath
 
 Start-Sleep -Seconds 5
 
 # Required to reload the PerUser settings
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters , 1 , True
 
 
 # Use the line below to verify that the wallpaper has been set
