27d442693c321811c1b65638e9401af9008f6821
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-09 21:54:00 +0100

    fix: point to correct nvim location on windows

--- nvim/install.ps1
+++ nvim/install.ps1
@@ -1,3 +1,4 @@
+$VIMHOME = "$env:LOCALAPPDATA\nvim"
 # Create Symbolic link for powershell profile
 Write-Verbose "Creating symbolic link for nvim config from $PSScriptRoot\config to $HOME\.config\nvim"
-New-Item -ItemType SymbolicLink -Path $HOME\.config\nvim -Target $PSScriptRoot\config -Force > $null
+New-Item -ItemType SymbolicLink -Path $VIMHOME -Target $PSScriptRoot\config -Force > $null
