655a63ece7a7f671cb5634d1ce571b1681494529
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-12 17:43:27 +0100

    feat: some awesome nvim keymaps

--- nvim/config/lua/stephanvs/remap.lua
+++ nvim/config/lua/stephanvs/remap.lua
@@ -3,4 +3,27 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 
+-- In visual-line mode, move selection up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Merge line-below but keep cursor in place
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in place during half-page (U)p / (D)own
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in middle during search navigation
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Greatest remap ever - keep yanked after paste
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Next greatest remap ever - yank to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
 
