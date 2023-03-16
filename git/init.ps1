e1ef54bc71bcdc152c2369cfb6f10ddd595e664f
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-16 17:42:05 +0100

    feat: git pull (gpl) alias on windows

--- git/init.ps1
+++ git/init.ps1
@@ -6,6 +6,7 @@ function Get-GitStatus { & git status }
 function Set-GitCommit { & git commit -m $args }
 function Set-GitAddAll { & git add --all }
 function Set-GitPush { & git push }
+function Set-GitPull { & git pull }
 
 Write-Verbose "Setting git aliases:"
 
@@ -13,5 +14,6 @@ Set-Alias -Name gs -Value Get-GitStatus -Force -Scope Global
 Set-Alias -Name gcm -Value Set-GitCommit -Force -Scope Global
 Set-Alias -Name gaa -Value Set-GitAddAll -Force -Scope Global
 Set-Alias -Name gp -Value Set-GitPush -Force -Scope Global
+Set-Alias -Name gpl -Value Set-GitPull -Force -Scope Global
 
 Write-Verbose "Git aliases installed."
