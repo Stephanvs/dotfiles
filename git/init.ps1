6f2b6b0dcd2b56bf6ec0598da4b4ea606f55205c
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-16 17:24:51 +0100

    feat: git add all alias on windows

--- git/init.ps1
+++ git/init.ps1
@@ -4,10 +4,12 @@ Param(
 
 function Get-GitStatus { & git status }
 function Set-GitCommit { & git commit -m $args }
+function Set-GitAddAll { & git add --all }
 
 Write-Verbose "Setting git aliases:"
 
 Set-Alias -Name gs -Value Get-GitStatus -Force -Scope Global
 Set-Alias -Name gcm -Value Set-GitCommit -Force -Scope Global
+Set-Alias -Name gaa -Value Set-GitAddAll -Force -Scope Global
 
 Write-Verbose "Git aliases installed."
