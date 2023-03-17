739867c3660ffef5f9db9a4d3f73d206916c12f3
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-17 13:26:48 +0100

    feat: choco tab completion

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -649,3 +649,12 @@ Set-PSReadLineKeyHandler -Key Ctrl+Shift+t `
   [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet test")
   [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
