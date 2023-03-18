da728db0ffa46d82be32ea9ecfa1ae4a13a7ff27
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-18 11:43:07 +0100

    fix: disable mapping to supress error

--- nvim/config/after/plugin/harpoon.lua
+++ nvim/config/after/plugin/harpoon.lua
@@ -4,6 +4,6 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", mark.jump_to_previous)
-vim.keymap.set("n", "<C-l>", mark.jump_to_next)
+-- vim.keymap.set("n", "<C-h>", mark.jump_to_previous)
+-- vim.keymap.set("n", "<C-l>", mark.jump_to_next)
 
