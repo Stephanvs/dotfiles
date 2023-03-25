0d56851a9b96acc239f1a5e8ec40e320edfafeb2
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-25 15:43:10 +0100

    chore: remove output line

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -4,7 +4,6 @@ alias zshrc='vim $HOME/.zshrc'
 source $DOTFILES/zsh/aliases.zsh
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo "Creating alias for pbcopy and pbpaste"
   alias pbcopy='xclip -selection clipboard'
   alias pbpaste='xclip -selection clipboard -o'
 fi
