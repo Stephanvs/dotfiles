b6bfe770692c4f331e2b5b2d57292ce34ee0fddd
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-29 13:10:19 +0200

    feat(harpoon): enable next/prev nav

--- nvim/config/after/plugin/harpoon.lua
+++ nvim/config/after/plugin/harpoon.lua
@@ -4,6 +4,5 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
--- vim.keymap.set("n", "<C-h>", mark.jump_to_previous)
--- vim.keymap.set("n", "<C-l>", mark.jump_to_next)
-
+vim.keymap.set("n", "<C-h>", ui.nav_prev)
+vim.keymap.set("n", "<C-l>", ui.nav_next)
