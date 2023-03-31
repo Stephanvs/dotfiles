4e12529f6359557db1cdd804dc177a964a76f18f
Stephan van Stekelenburg <stephan@hayman.io>
2023-03-31 20:12:55 +0200

    feat: lsp config

--- nvim/config/after/plugin/lsp.lua
+++ nvim/config/after/plugin/lsp.lua
@@ -1,11 +1,23 @@
 local lsp = require('lsp-zero')
 
-lsp.preset({
-	name = 'minimal',
-	set_lsp_keymaps = true,
-	manage_nvim_cmp = true,
-	suggest_lsp_servers = false,
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
+
+--lsp.preset({
+--	name = 'minimal',
+--	set_lsp_keymaps = true,
+--	manage_nvim_cmp = true,
+--	suggest_lsp_servers = false,
+--})
+lsp.preset("recommended")
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -45,7 +57,7 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("i", "<M-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
