b65ba59c2dfccb057d3df16fb0c3bf3853752667
Stephan van Stekelenburg <stephan@hayman.io>
2023-04-06 03:34:34 +0200

    fix: remap duplicate key binding

--- nvim/config/after/plugin/harpoon.lua
+++ nvim/config/after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<c-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", ui.nav_prev)
-vim.keymap.set("n", "<C-l>", ui.nav_next)
+vim.keymap.set("n", "<m-h>", ui.nav_prev)
+vim.keymap.set("n", "<m-l>", ui.nav_next)
