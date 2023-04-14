ad91b65b75b8f36a7f634d00b40f86062062097d
Stephan van Stekelenburg <stephan@hayman.io>
2023-04-14 22:09:58 +0200

    feat: pnpm init

--- js/init.zsh
+++ js/init.zsh
@@ -1,1 +1,9 @@
 source $DOTFILES/js/aliases.zsh
+
+# pnpm
+export PNPM_HOME="/Users/stephanvs/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
