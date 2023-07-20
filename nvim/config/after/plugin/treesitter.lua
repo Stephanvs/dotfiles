a1e7cc5db0d8ce7d0bbced2183f38bf553de4563
Stephan van Stekelenburg <stephan@hayman.io>
2023-07-20 21:32:05 +0200

    fix: help is replaced by vimdoc

--- nvim/config/after/plugin/treesitter.lua
+++ nvim/config/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "lua", "vim", "help", "query" },
+  ensure_installed = { "javascript", "typescript", "lua", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
