0021d899f5db2c601b20ebd6f0e63d3113f47849
Stephan van Stekelenburg <stephan@hayman.io>
2023-09-17 19:29:09 +0200

    feat(macos): support for nvm

--- js/init.zsh
+++ js/init.zsh
@@ -1,5 +1,10 @@
 source $DOTFILES/js/aliases.zsh
 
+# nvm
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+# end nvm
+
 # pnpm
 export PNPM_HOME="/Users/stephanvs/Library/pnpm"
 case ":$PATH:" in
