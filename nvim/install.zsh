5b5e9c9773012c1e4e09e58dff42aac236da4ae1
Stephan van Stekelenburg <stephan@hayman.io>
2023-10-20 22:28:56 +0200

    chore: restore previous nvim settings

--- nvim/install.zsh
+++ nvim/install.zsh
@@ -1,10 +1,11 @@
 # nuke nvim dir first
 rm -rf $HOME/.config/nvim
 
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+#
+# # link custom config folder inside nvim
+# ln -nfs $DOTFILES/nvim/custom $HOME/.config/nvim/lua/custom
 
 # create new symbolic link
-#ln -nfs $DOTFILES/nvim/config $HOME/.config/nvim
+ln -nfs $DOTFILES/nvim/config $HOME/.config/nvim
 
-# link custom config folder inside nvim
-ln -nfs $DOTFILES/nvim/custom $HOME/.config/nvim/lua/custom
