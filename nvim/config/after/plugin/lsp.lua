fe000234fd872d69d68266b348e5383a83493a11
Stephan van Stekelenburg <stephan@hayman.io>
2023-10-20 22:31:05 +0200

    feat: lsp mappings

--- nvim/config/after/plugin/lsp.lua
+++ nvim/config/after/plugin/lsp.lua
@@ -48,6 +48,7 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -57,7 +58,7 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<M-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "<leader>fh", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
--- nvim/config/after/plugin/telescope.lua
+++ nvim/config/after/plugin/telescope.lua
@@ -1,6 +1,24 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("grep> ") })
-end)
+
+-- Go to declaration
+-- vim.keymap.set(
+--     'n',
+--     '<leader>fD',
+--     vim.lsp.buf.declaration,
+--     { description = 'Go to declaration' })
+--
+-- -- Go to definition
+-- vim.keymap.set(
+--     'n',
+--     '<leader>fd',
+--     builtin.lsp_definitions,
+--     { description = 'Go to definition' })
+
+-- Find in files
+--vim.keymap.set('n', '<leader>ff', builtin.find_files, { description = 'Find all files' })
+--vim.keymap.set('n', '<leader>fg', builtin.git_files, { description = 'Find git files' })
+--vim.keymap.set('n', '<leader>fs', function() builtin.grep_string({ search = vim.fn.input("grep> ") }) end, { description = 'Find grep string' })
+
+--vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { description = 'Show hover' })
+--vim.keymap.set('n', '<leader>fi', function() vim.lsp.buf.implementation() end, { description = 'LSP Go to implementation' })
+--vim.keymap.set('n', '<leader>fs', function() vim.lsp.buf.signature_help() end, { description = 'LSP Signature help' })
