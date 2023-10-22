4140d69aea19d6f85f234c3a802d5517bfa1bd2a
Stephan van Stekelenburg <stephan@hayman.io>
2023-10-22 22:19:01 +0200

    feat: add mapping for lsp references

--- nvim/config/after/plugin/lsp.lua
+++ nvim/config/after/plugin/lsp.lua
@@ -48,8 +48,9 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+	vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "gr", '<cmd>Telescope lsp_references<cr>', opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
