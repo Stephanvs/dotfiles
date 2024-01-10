b2d6c2ed7a8fe8cac657c7bbac0f8edd08cb0ef2
Stephan van Stekelenburg <stephan@hayman.io>
2024-01-11 00:16:35 +0100

    feat: disable netrw banner

--- nvim/2024/init.lua
+++ nvim/2024/init.lua
@@ -1,6 +1,7 @@
 -- -- disable netrw
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
+vim.g.netrw_banner = 0 -- disable netrw banner
 
 require("stephanvs.remap")
 require("stephanvs.set")
