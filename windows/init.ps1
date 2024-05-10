ae1f9b5d11dbb15ed1a05aa9f4dea06d2e06775f
Stephan van Stekelenburg <stephan@hayman.io>
2024-05-10 12:35:57 +0200

    feat: pb-copy/pb-paste alias and usage of eza

--- windows/init.ps1
+++ windows/init.ps1
@@ -1,7 +1,9 @@
 Set-Alias pbcopy Set-Clipboard
 Set-Alias pbpaste Get-Clipboard
+Set-Alias pb-copy Set-Clipboard
+Set-Alias pb-paste Get-Clipboard
 
-function Get-AllItems { & ls }
+function Get-AllItems { & eza --icons --group-directories-first --sort size --all --no-filesize --no-permissions --no-time --no-quotes }
 
 
 Set-Alias -Name l -Value Get-AllItems -Force -Scope Global
