cf131025e64b3055c991e68c29215c81621e9ab9
Stephan van Stekelenburg <stephan@hayman.io>
2024-05-19 17:14:36 +0200

    feat: remove some apps that are now installed by scoop

--- windows/Install-Apps.ps1
+++ windows/Install-Apps.ps1
@@ -1,20 +1,9 @@
-# PowerShell Core
-winget install Microsoft.PowerShell --silent
-
-# Dev editors
-winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements
-winget install Microsoft.VisualStudio.2022.Professional --silent --accept-package-agreements --accept-source-agreements
-winget install JetBrains.Rider --silent
-
 # Generic tools
 winget install AgileBits.1Password --silent
-winget install GitHub.GitHubDesktop --silent
-winget install Git.Git --silent
 
 # PowerToys
-winget install XP89DCGQ3K6VLD --silent --accept-package-agreements --accept-source-agreements
+# winget install XP89DCGQ3K6VLD --silent --accept-package-agreements --accept-source-agreements
 
 # Communication tools
 winget install WhatsApp.WhatsApp --silent
-winget install SlackTechnologies.Slack --silent
 winget install Microsoft.Teams --silent
