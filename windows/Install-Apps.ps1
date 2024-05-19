356cb62c17955391cbe35820c26f4fc0399c9f48
Stephan van Stekelenburg <stephan@hayman.io>
2024-05-19 20:19:30 +0200

    feat: install arc browser, and add comment for more clarity

--- windows/Install-Apps.ps1
+++ windows/Install-Apps.ps1
@@ -1,9 +1,10 @@
 # Generic tools
 winget install AgileBits.1Password --silent
+winget install TheBrowserCompany.Arc --accept-package-agreements --silent
 
 # PowerToys
 # winget install XP89DCGQ3K6VLD --silent --accept-package-agreements --accept-source-agreements
 
 # Communication tools
-winget install WhatsApp.WhatsApp --silent
+winget install 9NKSQGP7F2NH --disable-interactivity --accept-package-agreements # WhatsApp
 winget install Microsoft.Teams --silent
