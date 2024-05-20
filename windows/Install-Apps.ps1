e317ffd1001f28cd8ad977fe81d6aec0bca573a9
Stephan van Stekelenburg <stephan@hayman.io>
2024-05-20 13:38:23 +0200

    feat: install anytype client

--- windows/Install-Apps.ps1
+++ windows/Install-Apps.ps1
@@ -1,6 +1,7 @@
 # Generic tools
 winget install AgileBits.1Password --silent
 winget install TheBrowserCompany.Arc --accept-package-agreements --silent
+winget install AnyAssociation.Anytype --silent
 
 # PowerToys
 # winget install XP89DCGQ3K6VLD --silent --accept-package-agreements --accept-source-agreements
