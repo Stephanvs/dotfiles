7dce9f7d4bc0d53c0356ff216900d769706b200a
Stephan van Stekelenburg <stephan@hayman.io>
2024-07-11 14:31:15 +0200

    feat: remove anytype and powertoys

--- windows/Install-Apps.ps1
+++ windows/Install-Apps.ps1
@@ -1,10 +1,6 @@
 # Generic tools
 winget install AgileBits.1Password --silent
 winget install TheBrowserCompany.Arc --accept-package-agreements --silent
-# winget install AnyAssociation.Anytype --silent
-
-# PowerToys
-# winget install XP89DCGQ3K6VLD --silent --accept-package-agreements --accept-source-agreements
 
 # Communication tools
 winget install 9NKSQGP7F2NH --disable-interactivity --accept-package-agreements # WhatsApp
