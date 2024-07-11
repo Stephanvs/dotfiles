1fe094e1d06e0520d88224c02aca390d483e321d
Stephan van Stekelenburg <stephan@hayman.io>
2024-07-11 17:13:14 +0200

    chore: update wallpaper

--- wallpapers/install.ps1
+++ wallpapers/install.ps1
@@ -1,4 +1,4 @@
-$Wallpaper = Resolve-Path "$PSScriptRoot/ana.jpg".ToLower()
+$Wallpaper = Resolve-Path "$PSScriptRoot/desert.jpg".ToLower()
 
 $current = Get-ItemPropertyValue -Path "HKCU:\Control Panel\Desktop" -Name WallPaper
 Write-Host "Current Wallpaper: $current"
