5cbf574bf57d84909924218a858817d239b34189
Stephan van Stekelenburg <stephan@hayman.io>
2025-02-17 14:11:11 +0100

    feat: vim config in nvchad + tmux navigation

--- nvim/nvchad/lua/mappings.lua
+++ nvim/nvchad/lua/mappings.lua
@@ -1,10 +1,42 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- #########
+-- Normal mode
+-- #########
+
+-- In visual-line mode, move selection up/down
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Merge line-below but keep cursor in place
+map("n", "J", "mzJ`z")
+
+-- Keep cursor in place during half-page (U)p / (D)own
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in middle during search navigation
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Greatest remap ever - keep yanked after paste
+map("x", "<leader>p", "\"_dP", { desc = "paste without yanking" })
+
+-- Next greatest remap ever - yank to clipboard
+map("n", "<leader>y", "\"+y", { desc = "yank to clipboard" })
+map("n", "<leader>Y", "\"+Y", { desc = "yank to clipboard" })
+map("v", "<leader>y", "\"+y", { desc = "yank to clipboard" })
+
+-- Window navigation
+-- map('n', '<c-h>', ':wincmd h<CR>')
+-- map('n', '<c-j>', ':wincmd j<CR>')
+-- map('n', '<c-k>', ':wincmd k<CR>')
+-- map('n', '<c-l>', ':wincmd l<CR>')
+
+-- j or k cancel insert mode
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- nvim/nvchad/lua/options.lua
+++ nvim/nvchad/lua/options.lua
@@ -1,6 +1,33 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.nu = true
+o.relativenumber = true
+
+-- indentation
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+
+o.smartindent = true
+o.wrap = false
+
+o.swapfile = false
+o.backup = false
+o.undofile = true
+
+o.scrolloff = 8
+o.signcolumn = "auto:4"
+-- o.isfname.append("@-@")
+
+o.updatetime = 50
+
+o.cursorcolumn = false
+o.cursorline = true
+o.cursorlineopt ='both' -- to enable cursorline!
+
+o.listchars = "space:·,tab:->,trail:-,nbsp:+"
+o.list = true
+
