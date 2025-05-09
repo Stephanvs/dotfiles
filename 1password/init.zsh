e57f99b9a8560358dbeec336f52cbe5c57ea04a6
Stephan van Stekelenburg <stephan@hayman.io>
2025-05-09 10:53:04 +0200

    feat: use correct sock location on macos

--- 1password/init.zsh
+++ 1password/init.zsh
@@ -1,2 +1,8 @@
 
-export SSH_AUTH_SOCK=~/.1password/agent.sock
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+fi;
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  export SSH_AUTH_SOCK=~/.1password/agent.sock
+fi
