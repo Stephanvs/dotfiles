6910db1972818647b1de6c62ea35e8c8f6803d8c
Stephan van Stekelenburg <stephan@hayman.io>
2025-08-22 09:16:01 +0200

    chore: disable python

--- python/init.zsh
+++ python/init.zsh
@@ -1,3 +1,3 @@
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
