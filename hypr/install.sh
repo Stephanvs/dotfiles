bf506b32b60606f02fc9f01f9f1051903b47904c
Stephan van Stekelenburg <stephan@hayman.io>
2026-01-08 10:51:03 +0100

    fix: syntax and use base folder

--- hypr/install.sh
+++ hypr/install.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
 # Create the destination directory if it doesn't exist
-mkdir -p "$HOME/.config/hypr"
-$dest = $HOME/.config/hypr
+dest="$HOME/.config/hypr"
+mkdir -p "$dest"
 
 for file in "$DOTFILES/hypr/"*.conf; do
   # check file existing
   [ -f "$file" ] || continue
 
-  echo "linking $file to $HOME/.config/hypr/$(basename "$file")"
+  echo "linking $file to $dest/$(basename "$file")"
   # create symlink
-  ln -sf "$file" "$HOME/.config/hypr/$(basename "$file")"
+  ln -sf "$file" "$dest/$(basename "$file")"
 done
