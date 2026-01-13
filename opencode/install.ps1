61d041a8f8a2cef6730fc9be3ce110621231c90a
Stephan van Stekelenburg <stephan@hayman.io>
2026-01-13 22:17:16 +0100

    fixes

--- opencode/install.ps1
+++ opencode/install.ps1
@@ -6,7 +6,7 @@ if (-not (Test-Path $HOME\.config\opencode)) {
 # Create symbolic links
 New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\opencode.json -Target $PSScriptRoot\opencode.json -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\prompts -Target $PSScriptRoot\prompts -Force | Out-Null
-New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\skills -Target $PSScriptRoot\skills -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\skill -Target $PSScriptRoot\skill -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\rules -Target $PSScriptRoot\rules -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\AGENTS.md -Target $PSScriptRoot\AGENTS.md -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path $HOME\.config\opencode\command -Target $PSScriptRoot\command -Force | Out-Null
--- opencode/install.zsh
+++ opencode/install.zsh
@@ -4,7 +4,7 @@ rm -rf $HOME/.config/opencode
 # create new symbolic link
 ln -nfs $DOTFILES/opencode/opencode.json $HOME/.config/opencode/opencode.json
 ln -nfs $DOTFILES/opencode/prompts $HOME/.config/opencode/prompts
-ln -nfs $DOTFILES/opencode/skills $HOME/.config/opencode/skills
+ln -nfs $DOTFILES/opencode/skill $HOME/.config/opencode/skill
 ln -nfs $DOTFILES/opencode/rules $HOME/.config/opencode/rules
 ln -nfs $DOTFILES/opencode/AGENTS.md $HOME/.config/opencode/AGENTS.md
 ln -nfs $DOTFILES/opencode/command $HOME/.config/opencode/command
