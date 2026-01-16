df0b3662789d41dcfa5d3af843c7f5525bf6018c
Stephan van Stekelenburg <stephan@hayman.io>
2026-01-16 17:43:51 +0100

    chore: rename skills dir

--- opencode/install.zsh
+++ opencode/install.zsh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.config/opencode
 # create new symbolic links
 ln -nfs $DOTFILES/opencode/opencode.json $HOME/.config/opencode/opencode.json
 ln -nfs $DOTFILES/opencode/prompts $HOME/.config/opencode/prompts
-ln -nfs $DOTFILES/opencode/skill $HOME/.config/opencode/skill
+ln -nfs $DOTFILES/opencode/skills $HOME/.config/opencode/skills
 ln -nfs $DOTFILES/opencode/rules $HOME/.config/opencode/rules
 ln -nfs $DOTFILES/opencode/AGENTS.md $HOME/.config/opencode/AGENTS.md
 ln -nfs $DOTFILES/opencode/command $HOME/.config/opencode/command
