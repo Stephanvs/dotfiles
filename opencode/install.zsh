7e50df94e7c215da31391d8abc3f3cad850463e9
Stephan van Stekelenburg <stephan@hayman.io>
2026-01-26 20:28:59 +0100

    opencode: symlink custom theme with no background

--- opencode/install.zsh
+++ opencode/install.zsh
@@ -9,5 +9,6 @@ ln -nfs $DOTFILES/opencode/opencode.json $HOME/.config/opencode/opencode.json
 ln -nfs $DOTFILES/opencode/prompts $HOME/.config/opencode/prompts
 ln -nfs $DOTFILES/opencode/skills $HOME/.config/opencode/skills
 ln -nfs $DOTFILES/opencode/rules $HOME/.config/opencode/rules
+ln -nfs $DOTFILES/opencode/themes $HOME/.config/opencode/themes
 ln -nfs $DOTFILES/opencode/AGENTS.md $HOME/.config/opencode/AGENTS.md
 ln -nfs $DOTFILES/opencode/command $HOME/.config/opencode/command
