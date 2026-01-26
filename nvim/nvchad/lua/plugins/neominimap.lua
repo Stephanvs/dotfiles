fa089e955ce51f0de3109488d71d4414580cbf7e
Stephan van Stekelenburg <stephan@hayman.io>
2026-01-26 15:28:25 +0100

    nvim: minimap

---
+++ nvim/nvchad/lua/plugins/neominimap.lua
@@ -0,0 +1,9 @@
+return {
+  "wfxr/minimap.vim",
+  lazy = false,
+  config = function()
+    vim.g.minimap_width = 10
+    vim.g.minimap_auto_start = 1
+    vim.g.minimap_auto_start_win_enter = 1
+  end,
+}
