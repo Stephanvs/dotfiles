a8a54693977a6785473fdb6ecf385ecccf601cb8
Stephan van Stekelenburg <stephan@hayman.io>
2026-02-06 20:09:00 +0100

    chore: cleanup of old nvim configs

--- nvim/2024/init.lua
+++
@@ -1,9 +0,0 @@
--- disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.netrw_banner = 0 -- disable netrw banner
-
-require("stephanvs.remap")
-require("stephanvs.set")
-require("stephanvs.shell")
-require("stephanvs")
--- nvim/config/after/plugin/harpoon.lua
+++
@@ -1,8 +0,0 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<c-e>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<m-h>", ui.nav_prev)
-vim.keymap.set("n", "<m-l>", ui.nav_next)
