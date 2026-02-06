46d0fba4d74bb6dff07f7241d5ccf4b853c73bad
Stephan van Stekelenburg <stephan@hayman.io>
2026-02-06 16:58:00 +0100

    chore: house keeping

--- xresources/install.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-ln -f -s $DOTFILES/xresources/profile $HOME/.profile
-ln -f -s $DOTFILES/xresources/Xresources $HOME/.Xresources
-
-# Copy xinitrc to .xinitrc, contains settings to initialize Xorg
-ln -f -s $DOTFILES/xresources/xinitrc $HOME/.xinitrc
