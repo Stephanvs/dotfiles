018f886516b3bb82bd311d78f51cdfa863bd4208
Stephan van Stekelenburg <stephan@hayman.io>
2026-02-24 09:03:40 +0100

    win: remove arc

--- windows/Install-Apps.ps1
+++ windows/Install-Apps.ps1
@@ -1,6 +1,5 @@
 # Generic tools
 winget install AgileBits.1Password --silent
-winget install TheBrowserCompany.Arc --accept-package-agreements --silent
 
 # Communication tools
 winget install 9NKSQGP7F2NH --disable-interactivity --accept-package-agreements # WhatsApp
