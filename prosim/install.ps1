ef55a524bf4623c27945788fcbb5e5d11be50a5c
Stephan van Stekelenburg <stephan@hayman.io>
2026-02-25 11:11:04 +0100

    prosim: log when env var cannot be set

--- prosim/install.ps1
+++ prosim/install.ps1
@@ -1,1 +1,6 @@
-[Environment]::SetEnvironmentVariable('HOST_IPV4', 'activation.prosim-ar.com', 'Machine')
+try {
+    [Environment]::SetEnvironmentVariable('HOST_IPV4', 'activation.prosim-ar.com', 'Machine')
+}
+catch {
+    Write-Error "Could not set HOST_IPV4 at Machine scope: $($_.Exception.Message)"
+}
