dfdb82e64fb99f986fe4c93aede8a5ddaae5635b
Stephan van Stekelenburg <stephan@hayman.io>
2026-02-25 11:10:35 +0100

    pwsh: remove unneeded -force param

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -1,3 +1,3 @@
-Import-Module -Name "$PSScriptRoot\..\lib\Symlink.psm1" -Force
+Import-Module -Name "$PSScriptRoot\..\lib\Symlink.psm1"
 
 New-Symlink -SourcePath "$PSScriptRoot/profile.ps1" -TargetPath "$([Environment]::GetFolderPath('MyDocuments'))/PowerShell/Microsoft.PowerShell_profile.ps1" -Label 'Profile link'
