07fa31c5520778610727404e5198b062b384142d
Stephan van Stekelenburg <stephan@hayman.io>
2026-02-26 10:10:17 +0100

    win: new alias for which and where

--- windows/init.ps1
+++ windows/init.ps1
@@ -6,3 +6,6 @@ Set-Alias pb-paste Get-Clipboard
 function Get-AllItems { & eza --icons --group-directories-first --sort size --all --no-filesize --no-permissions --no-time --no-quotes }
 
 Set-Alias -Name l -Value Get-AllItems -Force -Scope Global
+Remove-Alias -Name where -Force -Scope Global
+function where { Get-Command @args }
+Set-Alias -Name which -Value Get-Command -Force -Scope Global
