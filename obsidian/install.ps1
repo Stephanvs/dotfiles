f99b749b2853df4c22473f360c8e07d7eb7463cc
Stephan van Stekelenburg <stephan@hayman.io>
2026-03-03 17:23:03 +0100

    obsidian: install + config

---
+++ obsidian/install.ps1
@@ -0,0 +1,9 @@
+Import-Module -Name "$PSScriptRoot\..\lib\Symlink.psm1"
+
+$vaultPath = "$env:USERPROFILE\Documents\Obsidian Vault"
+if (-not (Test-Path -LiteralPath $vaultPath)) {
+    New-Item -ItemType Directory -Path $vaultPath -Force | Out-Null
+    Write-Host "Created Obsidian Vault directory at $vaultPath"
+}
+
+New-Symlink -SourcePath "$PSScriptRoot" -TargetPath "$vaultPath\.obsidian" -Label 'Obsidian config link'
