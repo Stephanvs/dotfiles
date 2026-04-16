6bc5fba6f0e96f9b14d3ec29b2b53c1e7c1401b3
Stephan van Stekelenburg <stephan@hayman.io>
2026-04-16 10:13:41 +0200

    git: updated config and global ignore

--- git/install.ps1
+++ git/install.ps1
@@ -1,3 +1,4 @@
 Import-Module -Name "$PSScriptRoot\..\lib\Symlink.psm1"
 
 New-Symlink -SourcePath "$PSScriptRoot/gitconfig" -TargetPath "$HOME/.gitconfig" -Label 'Git config link'
+New-Symlink -SourcePath "$PSScriptRoot/gitignore" -TargetPath "$HOME/.gitignore" -Label 'Git ignore link'
--- git/install.zsh
+++ git/install.zsh
@@ -2,3 +2,4 @@
 source $DOTFILES/lib/install.zsh
 
 symlink git/gitconfig "$HOME/.gitconfig"
+symlink git/gitignore "$HOME/.gitignore"
