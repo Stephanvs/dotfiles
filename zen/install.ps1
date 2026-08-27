d481de66f7e85cb51699113fc99f07582e03a120
Stephan van Stekelenburg <stephan@hayman.io>
2026-08-27 17:17:26 +0200

    zen: force new tab in existing window

--- zen/install.ps1
+++ zen/install.ps1
@@ -1,3 +1,4 @@
 Import-Module -Name "$PSScriptRoot\..\lib\Symlink.psm1"
 
 New-Symlink -SourcePath "$PSScriptRoot/zen-keyboard-shortcuts.json" -TargetPath "$HOME/scoop/persist/zen-browser/profile/zen-keyboard-shortcuts.json" -Label 'Zen keyboard shortcuts link'
+New-Symlink -SourcePath "$PSScriptRoot/user.windows.js" -TargetPath "$HOME/scoop/persist/zen-browser/profile/user.js" -Label 'Zen Windows preferences link'
---
+++ zen/user.windows.js
@@ -0,0 +1,4 @@
+// Keep external app links in the current Zen window as new tabs, even when Komorebi cloaks it.
+user_pref("browser.link.open_newwindow", 3);
+user_pref("browser.link.open_newwindow.override.external", 3);
+user_pref("widget.prefer_windows_on_current_virtual_desktop", false);
